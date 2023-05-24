9e85744d1bd6a8fe232d1a3a6b535cf6d86daa6c
Yevhenii Parfonov <94915439+Parfyonovevg@users.noreply.github.com>
2023-05-24 11:26:30 +0300

    added logic to play-button

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const burger = document.querySelector('.burger-menu');
 const navigation = document.querySelector('.navigation');
+const playButton = document.querySelector('.video__play-button');
+const video = document.querySelector('iframe');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
   navigation.classList.toggle('open');
 });
 
-
-console.log('hi')
+playButton.addEventListener('click', () => {
+  video.src = 'https://www.youtube.com/embed/xbqA6o8_WC0?autoplay=1';
+  playButton.style.display = 'none';
+  document.querySelector('.video__text').style.display = 'none';
+});
